ffd4c1c9f76deda44df9e30ca819dd7800a56974
emmanuel <segunbolawole@yahoo.com>
2022-12-20 12:47:28 +0100

    Fly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import data from './data'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/">DealDey</a>
       </header>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product)=>(
+          <div key={product.slug}>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
