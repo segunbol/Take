1b1719d3049dc3897c7daca2c15fec11ecfa325e
emmanuel <segunbolawole@yahoo.com>
2022-12-23 02:56:07 +0100

    Commit 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import data from './data'
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">DealDey</a>
+    <BrowserRouter>
+    <div className="d-flex flex-column site-container">
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Deal Dey</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+          </Navbar>
+        
       </header>
       <main>
-        <h1>Featured Products</h1>
-        {data.products.map((product)=>(
-          <div key={product.slug}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
+        <Container>
+          <Routes>
+            <Route path="/product/:slug" element={ <ProductScreen /> } />
+            <Route path="/" element={ <HomeScreen/> } />
+          </Routes>
+        </Container>
       </main>
+      <footer>
+        <div>All Rights Resolved</div>
+      </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
