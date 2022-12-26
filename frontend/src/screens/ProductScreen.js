e541cb479310f666bc131c174ed023be347f45ed
emmanuel <segunbolawole@yahoo.com>
2022-12-26 09:48:30 +0100

    Create React Context(Add to Cart)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import Rating from "../components/Rating";
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,6 +12,7 @@ import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
+import { Store } from "../Store";
 
 
 
@@ -50,6 +51,12 @@ function ProductScreen () {
           };
           fetchData();
       }, [slug]);
+      const {state, dispatch: ctxDispatch} = useContext(Store);
+      const addToCartHandler = () => {
+        ctxDispatch({
+            type: 'CART_ADD_ITEM', 
+            payload: {...product, quantity: 1}})
+      }
     return (
         loading? (<LoadingBox/>)
         : error? (<MessageBox variant="danger">{error}</MessageBox>)
@@ -106,7 +113,7 @@ function ProductScreen () {
                                 {product.countInStock > 0 && (
                                     <ListGroup.Item>
                                         <div className="d-grid">
-                                            <Button variant="primary">
+                                            <Button onClick={addToCartHandler} variant="primary">
                                                 Add To  Cart
                                             </Button>
                                         </div>
