afd346269396ba51f76a814fee3a98ed79122021
emmanuel <segunbolawole@yahoo.com>
2022-12-31 11:54:06 +0100

    Completed SignIn Screen

--- backend/data.js
+++ backend/data.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs'
 const data = {
   users: [
     {
-      name: 'Basir',
+      name: 'ExtraMortal',
       email: 'admin@example.com',
       password: bcrypt.hashSync('123456'),
       isAdmin: true,
